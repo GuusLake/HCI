5a15bb90138b7f2904614bae82ab46ec66ac47f9
nickalgra <nickalgra@hotmail.com>
2020-03-02 22:09:33 +0100

    now rebind double click event

--- part3.py
+++ part3.py
@@ -10,74 +10,22 @@ from tkinter import ttk
 import time
 import queue
 import threading
+from part2 import CommentTreeDisplay
 
-
-class CommentTreeDisplay(tk.Frame):
-    def __init__(self, parent, reddit):
-        tk.Frame.__init__(self, parent)
-        self.reddit = reddit
-        self.columnconfigure(0, weight=1)
-        
-        # Make menubar
-        self.menubar = tk.Menu(self)
-        self.filemenu = tk.Menu(self.menubar, tearoff=0)
-        self.filemenu.add_command(label="Load comments", command=self.loadCommentsPopup)
-        self.filemenu.add_command(label="exit", command=self.quit)
-        self.menubar.add_cascade(label="File", menu=self.filemenu)
-        parent.config(menu=self.menubar)
-        
-        # Create comment tree
-        self.commentTree = ttk.Treeview(self)
-        self.yscrollbarComment = ttk.Scrollbar(self, orient='vertical', command=self.commentTree.yview)
-        self.commentTree.configure(yscrollcommand=self.yscrollbarComment.set)
-        self.yscrollbarComment.grid(row=0, column=0, sticky='nse')
-        self.commentTree.grid(column=0, row=0, columnspan=3, sticky=tk.NSEW)
-        self.submisUrl = 'https://www.reddit.com/r/AskReddit/comments/fca671/what_has_always_been_your_fun_fact_when_asked/'
-        threading.Thread(target=self.showComments).start()
-    
-    def showComments(self):
-        self.newTree = ttk.Treeview(self)
-            
-        submission = self.reddit.submission(url=self.submisUrl)
-        submission.comments.replace_more(limit=0)
-        for comment in submission.comments:
-            try:
-                self.newTree.insert('', 'end', comment.id, text=comment.body)
-            except:
-                self.newTree.insert('', 'end', comment.id, text="CHARACTER ERROR")
-            self.recursiveTreeBuilder(comment, comment.id)
-            
-        self.commentTree = self.newTree
-        self.commentTree.grid(column=0, row=0, columnspan=3, sticky=tk.NSEW)
-            
-    def recursiveTreeBuilder(self, parent, parent_id):
-        for child in parent.replies:
-            try:
-                self.newTree.insert(parent_id, 'end', child.id, text=child.body)
-            except:
-                self.newTree.insert(parent_id, 'end', child.id, text="CHARACTER ERROR")
-            self.recursiveTreeBuilder(child, child.id)
-            
-    def loadCommentsPopup(self):
-        self.win= tk.Toplevel(self)
-        self.label=tk.Label(self.win, text="Enter an URL")
-        self.label.pack()
-        self.urlEntry=tk.Entry(self.win)
-        self.urlEntry.pack()
-        self.btn=tk.Button(self.win,text='Load comments',command=self.loadComments)
-        self.btn.pack()
-    
-    def loadComments(self):
-        self.submisUrl = self.urlEntry.get()
-        threading.Thread(target=self.showComments).start()
-        self.win.destroy()
 
 class ResponseCommentTreeDisplay(CommentTreeDisplay):
-    def __init__(self, parent, reddit, queue):
-        CommentTreeDisplay.__init__(self, parent, reddit, queue)
+    def __init__(self, parent, reddit):
+        CommentTreeDisplay.__init__(self, parent, reddit)
+        print("Init complete")
+        
+    def attachTree(self):
+        self.commentTree = self.newTree
+        self.commentTree.grid(column=0, row=0, columnspan=3, sticky=tk.NSEW)
         self.commentTree.bind("<Double-1>", self.addComment)
+        print("Attached Event")
     
     def addComment(self, event):
+        print("Double Click Detected")
         comment = reddit.comment(id = commentID)
         reply = simpledialog.askstring(title = "Add comment", prompt = "Type your comment below:")
         comment.reply(reply)
@@ -91,7 +39,7 @@ def main():
                          )
     
     root = tk.Tk()
-    ctd = CommentTreeDisplay(root, reddit)
+    ctd = ResponseCommentTreeDisplay(root, reddit)
     ctd.pack(fill=tk.BOTH, expand = True)
     
     root.mainloop()

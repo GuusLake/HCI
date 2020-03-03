a93dfbbc2191ba7b634c108ee9dd0ad610acd25f
nickalgra <nickalgra@hotmail.com>
2020-03-03 21:37:57 +0100

    made part 3 and 4 standalone as per the assignment

--- part3.py
+++ part3.py
@@ -11,7 +11,83 @@ from tkinter import simpledialog
 import time
 import queue
 import threading
-from part2 import CommentTreeDisplay
+
+
+class CommentTreeDisplay(tk.Frame):
+    ''' An interface to display comments of a reddit submission in a tree '''
+    def __init__(self, parent, reddit):
+        tk.Frame.__init__(self, parent)
+        self.reddit = reddit
+        self.columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        # Make menubar
+        self.menubar = tk.Menu(self)
+        self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="Load comments", command=self.loadCommentsPopup)
+        self.filemenu.add_command(label="exit", command=self.quit)
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
+        parent.config(menu=self.menubar)
+
+        # Create comment tree
+        self.commentTree = ttk.Treeview(self)
+        self.yscrollbarComment = ttk.Scrollbar(self, orient='vertical', command=self.commentTree.yview)
+        self.commentTree.configure(yscrollcommand=self.yscrollbarComment.set)
+        self.yscrollbarComment.grid(row=0, column=0, sticky='nse')
+        self.commentTree.grid(column=0, row=0, columnspan=3, sticky=tk.NSEW)
+        self.submisUrl = ''
+
+    def showComments(self):
+        ''' Build a tree of comments for the top level comments '''
+        self.newTree = ttk.Treeview(self)
+        self.submission.comments.replace_more(limit=0)
+        for comment in self.submission.comments:
+            try:
+                self.newTree.insert('', 'end', comment.id, text=comment.body)
+            except:
+                self.newTree.insert('', 'end', comment.id, text="CHARACTER ERROR")
+            # For each top level comment, recursively build trees for their replies
+            self.recursiveTreeBuilder(comment, comment.id)
+        self.attachTree()
+
+    def attachTree(self):
+        ''' After a new tree is built, replaces the old comment tree with the new one '''
+        self.commentTree = self.newTree
+        self.yscrollbarComment = ttk.Scrollbar(self, orient='vertical', command=self.commentTree.yview)
+        self.commentTree.grid(column=0, row=0, columnspan=3, sticky=tk.NSEW)
+        self.yscrollbarComment.grid(row=0, column=0, sticky='nse')
+        self.commentTree.configure(yscrollcommand=self.yscrollbarComment.set)
+
+    def recursiveTreeBuilder(self, parent, parent_id):
+        ''' Recursive method for building trees for replies '''
+        for child in parent.replies:
+            try:
+                self.newTree.insert(parent_id, 'end', child.id, text=child.body)
+            except:
+                self.newTree.insert(parent_id, 'end', child.id, text="CHARACTER ERROR")
+            self.recursiveTreeBuilder(child, child.id)
+
+    def loadCommentsPopup(self):
+        ''' Shows popup asking for a submission url '''
+        self.win= tk.Toplevel(self)
+        self.label=tk.Label(self.win, text="Enter an URL")
+        self.label.pack()
+        self.urlEntry=tk.Entry(self.win)
+        self.urlEntry.pack()
+        self.btn=tk.Button(self.win,text='Load comments',command=self.loadComments)
+        self.btn.pack()
+
+    def loadComments(self):
+        ''' After url has been given, checks validity and closes the window '''
+        self.submisUrl = self.urlEntry.get()
+        try:
+            # Check for valid reddit post
+            self.submission = self.reddit.submission(url=self.submisUrl)
+            # Build the tree for new post in seperate thread
+            threading.Thread(target=self.showComments).start()
+        except:
+            messagebox.showerror('Error', 'The URL was invalid')
+        self.win.destroy()
 
 
 class ResponseCommentTreeDisplay(CommentTreeDisplay):
